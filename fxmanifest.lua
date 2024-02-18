59cd48981e76f5452406a38f70a2b26ef3a310a8
James <barowikajames@gmail.com>
2024-02-18 14:45:58 +0000

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
+author 'Smeary'
+description 'Vehicle Spawner for Car Showroom'
+version '1.4.0'
+
+shared_scripts {
+    'config.lua'
+}
+client_scripts {
+   'showroom.lua'
+}
