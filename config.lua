783c3afced79fb498f6c461c19a7cf00620802cc
James <barowikajames@gmail.com>
2024-02-18 14:44:11 +0000

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,31 @@
+Config.Shops = {
+    ['pdm'] = {
+        ['ShowroomVehicles'] = {
+            [1] = {
+                coords = vector4(-2220.8020, -377.3829, 13.3636, 318.2395), -- where the vehicle will spawn on display
+                defaultVehicle = 'divo',                       -- Default display vehicle
+                chosenVehicle = 'ardent',                        -- Same as default but is dynamically changed when swapping vehicles
+            },
+            [2] = {
+                coords = vector4(-2216.3118, -381.1669, 13.1155, 319.9619),
+                defaultVehicle = '2019chiron',
+                chosenVehicle = 'schafter2'
+            },
+            [3] = {
+                coords = vector4(-2204.0576, -390.8713, 13.0475, 319.9346),
+                defaultVehicle = 'dbx',
+                chosenVehicle = 'schafter2'
+            },
+            [4] = {
+                coords = vector4(-2199.3215, -399.0747, 13.3638, 255.6793),
+                defaultVehicle = 'baller',
+                chosenVehicle = 'schafter2'
+            },
+            [5] = {
+                coords = vector4(-2199.3215, -399.0747, 13.3638, 255.6793),
+                defaultVehicle = 'baller',
+                chosenVehicle = 'schafter2'
+            },
+        },
+    },
+}
